e2c6645f3279d0b95c88e3306196e426bab01fe1
Victor Vidal <victorvidal08@hotmail.com>
2022-04-19 19:34:51 -0300

    adiciona req 4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from '../pages/Loading';
 
@@ -37,6 +38,9 @@ class Header extends React.Component {
         <h1>Trybetunes</h1>
         {loading && <Loading />}
         <h2 data-testid="header-user-name">{user.name}</h2>
+        <Link to="/search" data-testid="link-to-search">Pesquisar</Link>
+        <Link to="/favorites" data-testid="link-to-favorites">Favoritxs</Link>
+        <Link to="/profile" data-testid="link-to-profile">Perfil</Link>
       </header>
     );
   }
